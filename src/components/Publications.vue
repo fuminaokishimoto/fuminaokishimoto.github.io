a8c8b8d774c5edc4394a53e3af042d70ff9320c3
suecharo <suehiro619@gmail.com>
2020-04-08 11:14:10 +0900

    Fix publications

--- src/components/Publications.vue
+++ src/components/Publications.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="d-flex flex-column">
     <h2 class="title mb-2">
-      Publications [<a
+      Publications
+      <a
         href="https://scholar.google.com/citations?user=ug6E06wAAAAJ&hl=en"
         class="subtitle-1"
-        >Google Scholar</a
-      >]
+        >[Google Scholar]</a
+      >
     </h2>
     <div
       v-for="(item, i) in publications"
@@ -14,7 +15,7 @@
       :class="{ 'mt-2': i !== 0 }"
     >
       <h3 class="subtitle-1 font-weight-medium">[{{ item.year }}]</h3>
-      <ol class="ml-4">
+      <ol class="ml-4" :start="startNum(item.year)">
         <li
           v-for="(content, j) in item.contents"
           :key="j"
@@ -33,7 +34,19 @@ export default {
   computed: {
     ...mapGetters({
       publications: 'content/getPublications'
-    })
+    }),
+    startNum() {
+      return function(year) {
+        let number = 1
+        for (const item of this.publications) {
+          if (item.year === year) {
+            return number
+          } else {
+            number = number + item.contents.length
+          }
+        }
+      }
+    }
   }
 }
 </script>
